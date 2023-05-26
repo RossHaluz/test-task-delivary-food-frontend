7a314f4c348ea199d4cd59d5cafa0d88213260c3
Rostyslav Haluzinskiy <106879174+RossHaluz@users.noreply.github.com>
2023-05-26 22:08:06 +0300

    add

--- src/components/Count/Count.jsx
+++ src/components/Count/Count.jsx
@@ -1,14 +1,27 @@
-import { BiLeftArrowCircle, BiRightArrowCircle } from 'react-icons/bi';
+import { useState } from 'react';
+import { BiLeftArrowCircle, BiRightArrowCircle } from 'react-icons/bi'; 
 
 const Count = ({amount}) => {
+  const [count, setCount] = useState(amount);
+
+  const increment = () => {
+    setCount(prev => prev + 1)
+  }
+
+  const decrement = () => {
+    if(count === 0) {
+      return
+    }
+    setCount(prev => prev - 1)
+  }
 
   return (
     <div>
-      <button type="button">
+      <button type="button" onClick={decrement}>
         <BiLeftArrowCircle />
       </button>
-      <span>{amount}</span>
-      <button type="button">
+      <span>{count}</span>
+      <button type="button" onClick={increment}>
         <BiRightArrowCircle />
       </button>
     </div>
