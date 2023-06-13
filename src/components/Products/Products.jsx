9a7a52e35878482a19a19c7014d90d42ee70fb95
Rostyslav Haluzinskiy <rossgaluzinskiy@gmail.com>
2023-06-13 12:26:31 +0300

    add

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,14 +2,27 @@ import ProductItem from '../ProductItem';
 import { FoodsList, FoodListContainer } from './Products.styled';
 import Filter from '../Filter/Filter';
 import { useDispatch, useSelector } from 'react-redux';
-import { isLoading, selectCurrentPage, selectCurrentPageCategory, selectCurrentShop, selectFoods, selectPageQtyCategory, selectTotalPage } from 'redux/foods/selectors';
+import {
+  isLoading,
+  selectCurrentPage,
+  selectCurrentPageCategory,
+  selectCurrentShop,
+  selectFoods,
+  selectPageQtyCategory,
+  selectTotalPage,
+} from 'redux/foods/selectors';
 import { Oval } from 'react-loader-spinner';
-import { Pagination, Stack } from '@mui/material';
-import { changeCurrentPage, changeCurrentPageCategory } from 'redux/foods/slice';
+import { Pagination, Stack, PaginationItem } from '@mui/material';
+import {
+  changeCurrentPage,
+  changeCurrentPageCategory,
+} from 'redux/foods/slice';
 import { getFoodsCategory } from 'redux/foods/operetions';
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-const Products = () => {
+const Products = props => {
+  console.log(props);
   const items = useSelector(selectFoods).foods;
   const isLoadingPage = useSelector(isLoading);
   const page = useSelector(selectCurrentPage);
@@ -18,55 +31,91 @@ const Products = () => {
   const pageCategory = useSelector(selectCurrentPageCategory);
   const pageQtyCategory = useSelector(selectPageQtyCategory);
   const dispatch = useDispatch();
-console.log(items);
+  console.log(items);
 
   useEffect(() => {
-    if(currentShop === ''){
+    if (currentShop === '') {
       return;
     }
-    
-    dispatch(getFoodsCategory({ pageCategory, currentShop}))
-  }, [dispatch, pageCategory, currentShop])
 
+    dispatch(getFoodsCategory({ pageCategory, currentShop }));
+  }, [dispatch, pageCategory, currentShop]);
 
   return (
-   <> <FoodListContainer>
-   <Filter />
-   <FoodsList>
-     {isLoadingPage && !items ? (
-       <Oval
-         height={80}
-         width={80}
-         color="#2196f3"
-         wrapperStyle={{}}
-         wrapperClass=""
-         visible={true}
-         ariaLabel="oval-loading"
-         secondaryColor="#2196f3"
-         strokeWidth={2}
-         strokeWidthSecondary={2}
-       />
-     ) : (
-       items.map(({ title, img, desc, price, _id, count }) => {
-         return (
-           <ProductItem
-             key={_id}
-             id={_id}
-             title={title}
-             img={img}
-             desc={desc}
-             price={price}
-             count={count}
-           />
-         );
-       })
-     )}
-   </FoodsList>
- </FoodListContainer>
- <Stack m='0 auto' mt={5} mb={5}>
-  {!!pageQty && currentShop === '' ? <Pagination count={pageQty} page={page} onChange={(_, num) => dispatch(changeCurrentPage(num))} showFirstButton showLastButton/> : <Pagination count={pageQtyCategory} page={pageCategory} onChange={(_, num) => dispatch(changeCurrentPageCategory(num))} showFirstButton showLastButton/>}
- </Stack>
- </>
+    <>
+      {' '}
+      <FoodListContainer>
+        <Filter />
+        <FoodsList>
+          {isLoadingPage && !items ? (
+            <Oval
+              height={80}
+              width={80}
+              color="#2196f3"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+              ariaLabel="oval-loading"
+              secondaryColor="#2196f3"
+              strokeWidth={2}
+              strokeWidthSecondary={2}
+            />
+          ) : (
+            items.map(({ title, img, desc, price, _id, count }) => {
+              return (
+                <ProductItem
+                  key={_id}
+                  id={_id}
+                  title={title}
+                  img={img}
+                  desc={desc}
+                  price={price}
+                  count={count}
+                />
+              );
+            })
+          )}
+        </FoodsList>
+      </FoodListContainer>
+      <Stack m="0 auto" mt={5} mb={5}>
+        {!!pageQty && currentShop === '' ? (
+          <Pagination
+            count={pageQty}
+            page={page}
+            onChange={(_, num) => dispatch(changeCurrentPage(num))}
+            renderItem={item => {
+              console.log(item.page);
+              return (
+                <PaginationItem
+                  component={Link}
+                  to={`/?page=${item.page}`}
+                  {...item}
+                />
+              );
+            }}
+            showFirstButton
+            showLastButton
+          />
+        ) : (
+          <Pagination
+            count={pageQtyCategory}
+            page={pageCategory}
+            onChange={(_, num) => dispatch(changeCurrentPageCategory(num))}
+            renderItem={item => {
+              return (
+                <PaginationItem
+                  component={Link}
+                  to={`/?shop=${currentShop}&page=${item.page}`}
+                  {...item}
+                />
+              );
+            }}
+            showFirstButton
+            showLastButton
+          />
+        )}
+      </Stack>
+    </>
   );
 };
 
